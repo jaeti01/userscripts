dea81e1ac555da6a1a59b409bb52de5b68aeecc5
Till Jaeger <till.jaeger@gmail.com>
2022-10-13 15:02:15 +0200

    Add files via upload

---
+++ SOC_Prime_title_changer.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         SOC Prime Title Changer
+// @namespace    https://tdm.socprime.com/
+// @version      0.1
+// @description  A very simple script that changes page titles within SOC Prime platform to easier identify modules during a demo with multiple tabs open
+// @author       till.jaeger@gmail.com
+// @match        https://tdm.socprime.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+     var pathname = window.location.pathname.match(/\/(.*)\//);
+   //* alert(!pathname[1].startsWith("tdm"));
+    if (!pathname[1].startsWith("tdm")) {
+         document.title = pathname[1].toUpperCase();
+     }
+})();
